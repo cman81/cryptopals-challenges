133416fc36ee1748a3d754e8a4bd710d98817aa0
cman81 <cman81@users.noreply.github.com>
2014-08-16 00:05:55 -0400

    Update set1-challenge1.php
    Show all PHP errors
    Completed set 1, challenge 2!

--- set1-challenge1.php
+++ set1-challenge1.php
@@ -1,21 +1,15 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-  function hex_to_base64($hex) { // http://cryptopals.com/sets/1/challenges/1/
+  function hex_to_base64($hex) { // http://cryptopals.com/sets/1/challenges/2/
     $hex = str_split(strtolower($hex));
 
     // loop through characters to create an array of 4-digit binary words
     $binary_str = ''; // form really long binary string
     foreach ($hex as $char) {
       // convert A-F to integer
-      switch ($char) {
-        case 'a': $char = 10; break;
-        case 'b': $char = 11; break;
-        case 'c': $char = 12; break;
-        case 'd': $char = 13; break;
-        case 'e': $char = 14; break;
-        case 'f': $char = 15; break;
-        default: $char = intval($char);
-      }
+      $char = hex_to_int($char);
       // convert into 4-digit binary
       $divisor = 8;
       while ($divisor >= 1) {
@@ -53,9 +47,51 @@
     }
 
     // join to form string of base64 characters, return
-
     return $base64;
   }
 
-  // returns 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t' as expected!
-  print hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
+  /**
+   * Write a function that takes two equal-length buffers and produces their XOR combination.
+   * http://cryptopals.com/sets/1/challenges/2/
+   */
+  function fixed_xor($a, $b) {
+    if (strlen($a) != strlen($b)) {
+      return FALSE;
+    }
+
+    $a = str_split($a);
+    $b = str_split($b);
+    $result = '';
+
+    for ($i = 0; $i < count($a); $i++) {
+      // decode hex
+      $int_a = hex_to_int($a[$i]);
+      $int_b = hex_to_int($b[$i]);
+      // perform XOR
+      $int_result = $int_a ^ $int_b;
+      // encode result to hex
+      $result .= int_to_hex($int_result);
+    }
+
+    return $result;
+  }
+
+  function hex_to_int($hex) {
+    switch ($hex) {
+      case 'a': return 10;
+      case 'b': return 11;
+      case 'c': return 12;
+      case 'd': return 13;
+      case 'e': return 14;
+      case 'f': return 15;
+      default: return intval($hex);
+    }
+  }
+
+  function int_to_hex($int) {
+    $char_map = str_split('0123456789abcdef');
+    return $char_map[$int];
+  }
+
+  var_dump(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'));
+  var_dump(fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'));
